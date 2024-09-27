bc553535bd0f16b2a773c6ebea996cdcd3bb439b
echanpostt <echan5886@gmail.com>
2024-09-27 14:12:21 +0700

    a

--- kafe06.java
+++ kafe06.java
@@ -17,7 +17,6 @@ public class kafe06 {
         jumlah = sc.nextInt();
         System.out.println("masukkan anggota (true/false): ");
         keanggotaan = sc.nextBoolean();
-
         double hargaMenu = 0;
         switch (menu.toLowerCase()) {
             case "kopi":
@@ -36,20 +35,19 @@ public class kafe06 {
      switch (ukuranCup) {
         case 'S':
         break;
-
         case 'M':
              totalHarga += 0.25 * totalHarga;
         break;
-
         case 'L':
         totalHarga += 0.4 * totalHarga;
             break;
+            default:
+            System.out.println("ukuran cup tidak valid");
      }
      double diskon = keanggotaan ? 0.1 : 0;
      double nominalBayar = totalHarga - (totalHarga * diskon);
 
-
-     System.out.println("item pembelian: " + jumlah + "" + menu + "dengan ukuran cup" + ukuranCup);
+     System.out.println("item pembelian: " + jumlah + " " + menu + " dengan ukuran cup " + ukuranCup);
      System.out.println("nominal bayar: " + nominalBayar);
     }
 }
