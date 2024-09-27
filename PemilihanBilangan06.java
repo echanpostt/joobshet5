d926a758f8a772460fd5f6f6e1d2bf90d866b196
echanpostt <echan5886@gmail.com>
2024-09-27 11:21:03 +0700

    a

--- PemilihanBilangan06.java
+++ PemilihanBilangan06.java
@@ -1,5 +1,4 @@
 import java.util.Scanner;
-
 /**
  * PemilihanBilangan06
  */
@@ -7,20 +6,12 @@ public class PemilihanBilangan06 {
 
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        System.out.print("Masukkan sebuah angka: ");
+
+        System.out.println("masukkan sebuah angka: ");
         int angka = sc.nextInt();
 
-        if (angka % 2 == 0)
-        {
-            System.out.println("Angka " + angka + " adalah bilangan genap");
-        }
-        else
-        {
-            System.out.println("Angka " + angka + " adalah bilangan ganjil");
-        }
+        String hasil = (angka % 2 == 0) ? "bilangan genap" : "bilangan ganjil";
 
-    
-
-
+        System.out.println(angka + " adalah " + hasil);
     }
 }
--- PemilihanHari06.java
+++ PemilihanHari06.java
@@ -1,28 +1,33 @@
 import java.util.Scanner;
 
+/**
+ * PemilihanHari06
+ */
 public class PemilihanHari06 {
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        String dayName, dayType;
-        System.out.print("Input day name: ");
-        dayName = sc.nextLine();
 
-        switch (dayName.toLowerCase()) {
-            case "monday" :
-            case "tuesday":
-            case "wednesday":
-            case "thursday":
-            case "friday":
-                dayType = "weekday";
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+        String  dayType;
+        int dayName;
+        System.out.print("Input day name: ");
+        dayName = sc.nextInt();
+        switch (dayName) {
+            case 1:
+            case 2:
+            case 3:
+            case 4:
+            case 5:
+                dayType = "weekdays";
                 break;
-            case "saturday":
-            case "sunday":
-                dayType = "wekeend";
-                break;     
+            case 6:
+            case 7:
+                dayType = "weekend";
             default:
-                dayType = "invalid day name";
+                dayType = "invalid lek";
         }
         System.out.println(dayName + " is a " + dayType);
-
+        sc.close();
     }
 }
+    
